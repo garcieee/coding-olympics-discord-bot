a7e2d12a23b0c9a329c1dc8dc83a94fd9cfbe23e
Jude Joseph Garcia Jr. <93895463+garcieee@users.noreply.github.com>
2025-09-27 17:00:37 +0800

    member tracker

---
+++ Core/__init__.py
@@ -0,0 +1,12 @@
+"""
+Core package for the Coding Olympics Discord Bot.
+
+This package contains the core functionality for the bot,
+including leaderboard management and other core features.
+"""
+
+# Import core modules to make them available when importing the package
+from .leaderboard import *
+from .compile_members import *
+
+__version__ = "1.0.0"
